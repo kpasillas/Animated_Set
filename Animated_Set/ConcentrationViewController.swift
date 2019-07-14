575203a7e900e1fda626f61ce1f16feca13cc6bb
Kristopher Pasillas <kris@kris-p.net>
2019-07-14 08:28:35 -0700

    Changed colors of Concentration from Halloween to neutral theme

--- Animated_Set/ConcentrationViewController.swift
+++ Animated_Set/ConcentrationViewController.swift
@@ -45,10 +45,10 @@ class ConcentrationViewController: UIViewController {
             scoreLabel.text = "Score: \(game.score)"
             if card.isFaceUp {
                 button.setTitle(emoji(for: card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
             }
         }
     }
